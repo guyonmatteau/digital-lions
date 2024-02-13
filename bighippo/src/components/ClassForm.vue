a497592ada0bc39edea91a1e94319640b84c40f6
Anne Lohmeijer <anne.lohmeijer@ah.nl>
2024-02-13 09:47:51 +0200

    First commit of frontend

---
+++ bighippo/src/components/ClassForm.vue
@@ -0,0 +1,17 @@
+<template>
+    <form>
+        <label>Name:</label>
+        <input type="text" v-model="name">
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name: ''
+            }
+        }
+    }
+</script>
+
