1b624c26e4ea2901515d0ca079aac6180c06d859
Anne Lohmeijer <anne.lohmeijer@ah.nl>
2024-02-14 10:21:37 +0200

    Start with API structure

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,12 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
-from routers.workshop import workshop_router
-from routers.user import user_router
-from routers.health import health_router
+from routers.workshop import router as workshop_router
+from routers.user import router as user_router
+from routers.health import router as health_router
 
 app = FastAPI(title="Big Hippo API")
 
 app.include_router(health_router, tags=["health"])
 app.include_router(workshop_router, tags=["workshop"])
-app.include_router(user_router, tags["user"])
-
-
-def start():
-    """Launched with `poetry run start` at root level"""
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+app.include_router(user_router, tags=["user"])
--- backend/app/routers/health.py
+++ backend/app/routers/health.py
@@ -5,7 +5,7 @@ router = APIRouter(prefix="/health")
 
 
 @router.get(
-    "/", response_description="Health check", summary="Health check", status_code=200
+    "", response_description="Health check", summary="Health check", status_code=200
 )
 async def get_health(request: Request):
     return JSONResponse(status_code=status.HTTP_200_OK, content={"status": "ok"})
