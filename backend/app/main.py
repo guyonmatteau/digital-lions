a09ac7f96878fd074e1d6a7ce1654cfd44324099
Anne Lohmeijer <anne.lohmeijer@ah.nl>
2024-03-27 19:12:12 +0100

    Add postgresdb and connect to API

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,8 +5,12 @@ from routers.workshop import router as workshop_router
 from routers.user import router as user_router
 from routers.health import router as health_router
 
-app = FastAPI(title="Big Hippo API")
+app = FastAPI(title="Digital Lion API", version="0.1.0")
+
+@app.on_event("startup")
+def startup_db_client():
+    pass
 
 app.include_router(health_router, tags=["health"])
 app.include_router(workshop_router, tags=["workshop"])
-app.include_router(user_router, tags=["user"])
+app.include_router(user_router, tags=["users"])
