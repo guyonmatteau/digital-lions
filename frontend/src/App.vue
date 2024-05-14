8613c558ce2d55ad6ee6e7f55ca2d06791520b35
Anne Lohmeijer <anne.lohmeijer@ah.nl>
2024-05-14 21:16:54 +0200

    Work in progress

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,20 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
-import Navigation from '@/components/Navigation.vue'
-</script>
 <template>
-  <body>
-    <div>
-      <Navigation />
-    </div>
-    <div>
-      <RouterView />
-    </div>
-  </body>
+  <img alt="Little Lions logo" src="./assets/logo.png" />
+  <LoginView/>
 </template>
+<script setup lang="ts">
+import LoginView from './views/Login.vue'
+</script>
+<style>
+img {
+  height: 200px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <form name="login-form" @submit.prevent="login(username, password)">
+    <div class="mb-3">
+      <label for="username">Username: </label>
+      <input id="username" v-model="username" type="text" />
+    </div>
+    <div class="mb-3">
+      <label for="password">Password: </label>
+      <input id="password" v-model="password" type="password" />
+    </div>
+    <button class="btn btn-outline-dark" type="submit">
+      Login
+    </button>
+  </form>
+</template>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+const username = ref('')
+const password = ref('')
+function login(username: string, password: string) {
+  console.log("Logged in with", username, password)  // Login logic here
+}
+</script>
