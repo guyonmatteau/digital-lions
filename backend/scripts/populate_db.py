786e1f46d88dd36b39d7f0b470f9769481feef18
Anne Lohmeijer <anne.lohmeijer@ah.nl>
2024-10-07 20:09:02 +0200

    Add frontend deployment

--- backend/scripts/populate_db.py
+++ backend/scripts/populate_db.py
@@ -18,6 +18,7 @@ logger.addHandler(handler)
 
 LOCALE = "zu_ZA"
 URL = "http://localhost:8000/api/v1"
+URL = "https://backend-dev-f90d.up.railway.app/api/v1"
 # URL = "https://backend-production-7bbc.up.railway.app/api/v1"
 COMMUNITY_COUNT = 3
 TEAM_COUNT = 3
@@ -31,46 +32,46 @@ if __name__ == "__main__":
 
     logger.info(f"Adding {COMMUNITY_COUNT} communities to the db")
 
-    # for _ in range(COMMUNITY_COUNT):
-    #     community = {"name": fake.first_name()}
-    #     logger.info(f"Creating community {community['name']}")
-    #     response = requests.post(f"{URL}/communities", json=community)
-    #     logger.info(response.json())
-    #     response.raise_for_status()
-    #     logger.info(
-    #         f"Community {community['name']} created with id {response.json()['id']}"
-    #     )
+    for _ in range(COMMUNITY_COUNT):
+        community = {"name": fake.first_name()}
+        logger.info(f"Creating community {community['name']}")
+        response = requests.post(f"{URL}/communities", json=community)
+        logger.info(response.json())
+        response.raise_for_status()
+        logger.info(
+            f"Community {community['name']} created with id {response.json()['id']}"
+        )
 
-    # community_ids = [r["id"] for r in requests.get(f"{URL}/communities").json()]
-    #
-    # logger.info(f"Adding {TEAM_COUNT} teams to the db")
-    # for _ in range(TEAM_COUNT):
-    #     team = {
-    #         "name": f"Team {fake.first_name()}",
-    #         "community_id": random.choice(community_ids),
-    #         "children": [
-    #             {
-    #                 "first_name": fake.first_name(),
-    #                 "last_name": fake.last_name(),
-    #                 "gender": random.choice(["male", "female", None]),
-    #                 **random.choice(
-    #                     [
-    #                         {"age": random.choice([random.randint(5, 18), None])},
-    #                         {
-    #                             "dob": f"{random.randint(2000, 2020)}-{random.randint(10, 12)}-{random.randint(10, 30)}"
-    #                         },
-    #                     ],
-    #                 ),
-    #             }
-    #             for _ in range(CHILD_COUNT)
-    #         ],
-    #     }
-    #     logger.info(f"Creating team {team}")
-    #     try:
-    #         response = requests.post(f"{URL}/teams", json=team)
-    #         response.raise_for_status()
-    #     except requests.exceptions.HTTPError as exc:
-    #         logger.error(f"Error creating team: {response.text}")
+    community_ids = [r["id"] for r in requests.get(f"{URL}/communities").json()]
+
+    logger.info(f"Adding {TEAM_COUNT} teams to the db")
+    for _ in range(TEAM_COUNT):
+        team = {
+            "name": f"Team {fake.first_name()}",
+            "community_id": random.choice(community_ids),
+            "children": [
+                {
+                    "first_name": fake.first_name(),
+                    "last_name": fake.last_name(),
+                    "gender": random.choice(["male", "female", None]),
+                    **random.choice(
+                        [
+                            {"age": random.choice([random.randint(5, 18), None])},
+                            {
+                                "dob": f"{random.randint(2000, 2020)}-{random.randint(10, 12)}-{random.randint(10, 30)}"
+                            },
+                        ],
+                    ),
+                }
+                for _ in range(CHILD_COUNT)
+            ],
+        }
+        logger.info(f"Creating team {team}")
+        try:
+            response = requests.post(f"{URL}/teams", json=team)
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as exc:
+            logger.error(f"Error creating team: {response.text}")
 
     """
     Add workshops to teams.
